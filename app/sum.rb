9d9871043c6a15db31f49e258a4b2a5bbd1eaea0
jkahne <jkahne@nimbleriver.com>
2015-06-22 20:32:52 -0400

    basic setup

--- app/sum.rb
+++ app/sum.rb
@@ -1,2 +1,9 @@
 class Sum
+  attr_accessor :numbers, :operators
+
+  def initialize
+    @numbers = %w[1 2 3 4 5 6 7 8 9]
+    @operators =  [ '+', '-',  ''] 
+  end
+
 end
--- test/app/sum_test.rb
+++ test/app/sum_test.rb
@@ -2,4 +2,10 @@ require_relative '../test_helper'
 
 describe Sum do
 
+  it 'test setup' do
+    s = Sum.new
+    assert_equal %w[1 2 3 4 5 6 7 8 9], s.numbers
+    assert_equal ['+', '-', ''], s.operators
+  end
+
 end
