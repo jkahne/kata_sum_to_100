e9a2ee63497b883c00551c56e1dd5680345e6ae5
jkahne <jkahne@nimbleriver.com>
2015-06-22 20:40:35 -0400

    to_s and basic eval

--- app/sum.rb
+++ app/sum.rb
@@ -6,4 +6,29 @@ class Sum
     @operators =  [ '+', '-',  ''] 
   end
 
+  def ev
+    eval(self.to_s)
+  end
+
+  def to_s
+    str = ''
+    str << @numbers[0]
+    str << @operators[0]
+    str << @numbers[1]
+    str << @operators[0]
+    str << @numbers[2]
+    str << @operators[0]
+    str << @numbers[3]
+    str << @operators[0]
+    str << @numbers[4]
+    str << @operators[0]
+    str << @numbers[5]
+    str << @operators[0]
+    str << @numbers[6]
+    str << @operators[0]
+    str << @numbers[7]
+    str << @operators[0]
+    str << @numbers[8]
+    str
+  end
 end
--- test/app/sum_test.rb
+++ test/app/sum_test.rb
@@ -8,4 +8,16 @@ describe Sum do
     assert_equal ['+', '-', ''], s.operators
   end
 
+  it '#to_s for starting setup (add everything)' do
+    s = Sum.new
+    assert_equal "1+2+3+4+5+6+7+8+9", s.to_s
+  end
+
+  it 'evaluates starting setup (add everything)' do
+    s = Sum.new
+    assert_equal 45, s.ev
+  end
+
+
+
 end
