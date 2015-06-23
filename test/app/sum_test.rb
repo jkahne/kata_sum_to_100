72cff44d9f2a56043dc9fee77826cf8b9ec557cb
jkahne <jkahne@nimbleriver.com>
2015-06-22 20:42:08 -0400

    small refactor

--- test/app/sum_test.rb
+++ test/app/sum_test.rb
@@ -2,19 +2,18 @@ require_relative '../test_helper'
 
 describe Sum do
 
+  let(:s) { Sum.new }
+
   it 'test setup' do
-    s = Sum.new
     assert_equal %w[1 2 3 4 5 6 7 8 9], s.numbers
     assert_equal ['+', '-', ''], s.operators
   end
 
   it '#to_s for starting setup (add everything)' do
-    s = Sum.new
     assert_equal "1+2+3+4+5+6+7+8+9", s.to_s
   end
 
   it 'evaluates starting setup (add everything)' do
-    s = Sum.new
     assert_equal 45, s.ev
   end
 
