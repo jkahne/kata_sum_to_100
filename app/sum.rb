ee4bedf7e927868df19c91c1dfdf550011b0bd7d
jkahne <jkahne@nimbleriver.com>
2015-06-22 22:01:02 -0400

    clean up

--- app/sum.rb
+++ app/sum.rb
@@ -13,38 +13,21 @@ class Sum
 
   def to_s
     str = ''
-
     str << @numbers[0]
-    # inc every 3*3*3*3*3*3*3
     str << @operators[idx(7)]
     str << @numbers[1]
-    # inc every 3*3*3*3*3*3
     str << @operators[idx(6)]
     str << @numbers[2]
-    # inc every 3*3*3*3*3
     str << @operators[idx(5)]
     str << @numbers[3]
-    # inc every 3*3*3*3
     str << @operators[idx(4)]
     str << @numbers[4]
-    # inc every 3*3*3
     str << @operators[idx(3)]
-
     str << @numbers[5]
-    # should inc once every time the final position goes through all operations 
-    # times the next-to-final operation going through all operations
-    # for operations ['+', '-', ''], that's 3*3
     str << @operators[idx(2)]
     str << @numbers[6]
-    # should inc once for every time the final operation goes through all operations
-    # should increment when counter is 3, 6, 9, etc
-    #
-    # this blows up once cx is 9.  need to cap it at @operators.length
-    # need to cap others as well. 
     str << @operators[idx(1)]
     str << @numbers[7]
-    # first to increment. this position will change every time inc is called.   
-    # go to next char in @operators
     str << @operators[idx(0)]
     str << @numbers[8]
     str
